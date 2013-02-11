43a3a659552b4697bf67ebad191374b7125a6bee
John Daniel <daniel@bosser.com>
2013-02-11 13:37:32 +0100

    Makes a listview, changes background

--- src/john/daniel/lek/MainActivity.java
+++ src/john/daniel/lek/MainActivity.java
@@ -3,59 +3,52 @@ package john.daniel.lek;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	int toggle;
-	Button button1, sharedPref;
-	TextView text;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //Skapa lite variabler och koppla ihop dem med saker i xml
-        toggle = 0;
-        button1 = (Button) findViewById(R.id.button);
-        text = (TextView) findViewById(R.id.text);
-        sharedPref = (Button) findViewById(R.id.sharedPref);
+        ListView listView = (ListView) findViewById(R.id.mylist);
+        String[] values = new String[] { "Aktiviteter", "SharedPreferences" };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter); 
         
-        //Sök efter ett knapptryck på "button1"
-        button1.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				//Ignorera det som har med toggle att göra. 
-				toggle++;
-				text.setText("" + toggle);
-				//Starta aktiviteten "NewScreen.class"
-				Intent intent = new Intent(MainActivity.this, NewScreen.class); 
-				startActivity(intent);
-			}
-		});
-        
-        //Knapp SharedPreferences och knapp ontrycksökare.
-        sharedPref.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				//Starta aktiviteten "SharedPrefs.class"
-				Intent intent = new Intent(MainActivity.this, SharedPref.class);
-				startActivity(intent);
-			}
-		});
-    }
-    @Override
-    //Jag vet inte vad det här är. 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+        listView.setOnItemClickListener(new OnItemClickListener() {
+        	  @Override
+        	  public void onItemClick(AdapterView<?> parent, View view,
+        	    int position, long id) {
+        	    Toast.makeText(getApplicationContext(),
+        	      "Click ListItem Number " + position, Toast.LENGTH_LONG)
+        	      .show();
+        	    
+        	    if (position == 0){
+        	    	Intent intent = new Intent(MainActivity.this, NewScreen.class);
+        	    	startActivity(intent);
+        	    }
+        	    else if (position == 1){
+        	    	Intent intent = new Intent(MainActivity.this, SharedPref.class);
+        	    	startActivity(intent);
+        	    }
+        	  }
+        	}); 
     }
 }
--- src/john/daniel/lek/SharedPref.java
+++ src/john/daniel/lek/SharedPref.java
@@ -1,7 +1,5 @@
 package john.daniel.lek;
 
-import java.util.Map;
-
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
